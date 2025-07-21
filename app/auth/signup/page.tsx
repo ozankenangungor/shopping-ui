27e3543e6d0ecc2154700441c7c45fc4093e2bdd
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-07-21 09:38:02 +0300

    crud operations

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,13 +1,39 @@
+"use client";
+
 import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
+import { useActionState } from "react"; // ✅ useFormState yerine
+import createUser from "./create-user";
 
 export default function Signup() {
-    return (
-        <Stack spacing={2} className="w-full max-w-xs">
-            <TextField label="Email" variant="outlined" type="email"/>
-            <TextField label="Password" variant="outlined" type="password" />
-            <Button variant="contained">Signup</Button>
-            <Link component={NextLink} href="/auth/login" className="self-center">Login</Link>
-        </Stack>
-  )
+  const [state, formAction] = useActionState(createUser, { error: "" });
+  console.log(state)
+  return (
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField
+          name="email"
+          label="Email"
+          variant="outlined"
+          type="email"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          variant="outlined"
+          type="password"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <Button type="submit" variant="contained">
+          Signup
+        </Button>
+        <Link component={NextLink} href="/auth/login" className="self-center">
+          Login
+        </Link>
+      </Stack>
+    </form>
+  );
 }
